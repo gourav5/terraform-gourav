f063e1eb21f8cac580466505d42fa422f378d4f9
gourav5 <33307086+gourav5@users.noreply.github.com>
2023-08-08 16:23:46 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0022f774911c1d690"
+  default     = "ami-0f5ee92e2d63afc18"
 }
 
 
