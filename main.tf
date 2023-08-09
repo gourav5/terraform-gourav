06803c4638b9ec2c88a5e585243a54eb1b1b3ce6
gourav5 <33307086+gourav5@users.noreply.github.com>
2023-08-09 11:02:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "WEB_public" {
   key_name                    = var.key_name
   security_groups             = ["${aws_security_group.ssh-security-group.id}"]
   subnet_id                   = aws_subnet.public-subnet-1.id
+  user_data_path              = var.user_data_path
   associate_public_ip_address = true
   #user_data                   = "${data.template_file.provision.rendered}"
   #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
