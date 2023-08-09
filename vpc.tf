7821efd5bd6c6a9afb28e9962bd2fb10b3c07445
gourav5 <33307086+gourav5@users.noreply.github.com>
2023-08-09 15:34:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -63,21 +63,21 @@ resource "aws_security_group" "ssh-security-group" {
   name        = "SSH Security Group"
   description = "Enable SSH access on Port 22"
   vpc_id      = aws_vpc.vpc.id
+
   ingress {
     description = "SSH Access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+}
    ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    
-  }
+  
   egress {
     from_port   = 0
     to_port     = 0
